137fd28b62e44922228a1ef9f382a131c6476da4
Kyle <kylespinaio@hotmail.com>
2021-02-19 19:16:23 -0500

    added to lower case.

--- Files/index.js
+++ Files/index.js
@@ -3,12 +3,12 @@ function randomColor() {
  var colors = ["blue", 'red', 'green', 'orange', 'yellow', 'purple', 'brown', 'white', 'black', 'tan'];
  
  let userColor = propmt("please choose a color")
- 
+ let userChoice = userColor.toLowerCase()
  let computerColor = colors[[Math.floor(Math.random()*colors.length)]];
 
- if (userColor == computerColor){
+ if (userChoice == computerColor){
         alert("yeah you matched")
- }else if (userColor !== computerColor){
+ }else if (userChoice !== computerColor){
      alert("sorry you didn't match")
  }else{
      "oop you for got to pick a color"
